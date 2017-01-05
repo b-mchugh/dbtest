f74cae81048b2538d511a2d61ffc1499b3582b71
Brendan Mchugh <bmchugh@redhat.com>
2017-01-05 12:13:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import socket
-import pip
+import psycopg2
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 hostname = socket.gethostname()
 IP = socket.gethostbyname(hostname)
 
+conn = psycopg2.connect("dbname=sampledb user=userEYQ password=ch6YJjwttWXGbk1S host=172.30.171.226 port=5432")
+cur = conn.cursor()
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+cur.execute("SELECT * FROM test;")
+stuff = cur.fetchone()
+conn.commit()
+cur.close()
+conn.close()
+	
 class RH(BaseHTTPRequestHandler):
 	def do_GET(self):
 		self.send_response(200)
@@ -13,7 +23,7 @@ class RH(BaseHTTPRequestHandler):
 		message = ['myapp test1' + 'Client: ' + self.address_string() + '<br>',
 					'Server: ' + IP + ' aka ' + hostname + '<br>',
 					'Date: ' + self.date_time_string() + '<br>']
-		message.append(str(pip.get_installed_distributions(local_only=True)))
+		message.append(str(stuff))
 		for i in message:
 			print(i)
 			self.wfile.write(bytes(i, "utf8"))
